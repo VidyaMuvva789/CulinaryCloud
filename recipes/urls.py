7be4769573cc21c5697f77474ea4a2b3ecb925d7
VidyaMuvva789 <144550928+VidyaMuvva789@users.noreply.github.com>
2024-12-12 17:21:47 +0530

    Update urls.py

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home , name="recipe-home"),
-    path('about/', views.about , name="recipe-about"),
+    path('', views.home , name="recipes-home"),
+    path('about/', views.about , name="recipes-about"),
 ]
