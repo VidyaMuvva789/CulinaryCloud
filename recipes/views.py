08b5669a97a2d15f3c6fb2b481ca618722e82031
VidyaMuvva789 <144550928+VidyaMuvva789@users.noreply.github.com>
2024-12-12 17:22:19 +0530

    Update views.py

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render,HttpResponse
 
 # Create your views here.
+
+recipes=[
+    {
+        'author' : 'Dom',
+        'title' : 'metaball sub',
+        'directions' : 'combine all ingredients',
+        'date_posted' : 'May 19, 2022'
+    },
+    {
+        'author' : 'Dom',
+        'title' : 'turkey sub',
+        'directions' : 'combine all ingredients',
+        'date_posted' : 'May 16, 2022'
+    }
+]
+
 def home(request):
-    return  render(request,'home.html')
+    context = {
+        'recipes' : recipes
+    }
+    return  render(request,'home.html',context)
 
 def about(request):
-    return  render(request,'about.html')
+    return  render(request,'about.html',{'title':'about us page'})
